5ffc83437c5720d2cb415efd77d564eaf3bd02fa
unknown <el_word9@yahoo.com>
2024-03-15 17:11:03 -0500

    Added resume download and i added about me

--- Main/src/components/About/index.jsx
+++ Main/src/components/About/index.jsx
@@ -6,10 +6,14 @@ function About() {
         <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>
         </div>
         <p>
-        Helvetica put a bird on it church-key Blue Bottle banjo bespoke brunch Etsy authentic Marfa quinoa typewriter plaid direct trade small batch wayfarers bicycle rights cliche craft beer gastropub single-origin coffee Godard Carles you probably haven't heard of them irony pickled kitsch synth sriracha gentrify literally heirloom blog Truffaut paleo scenester
-        </p>
+        As I prepare to graduate from Southern Methodist University (SMU), I find myself standing at the threshold of an exciting new phase in my Software Engineering journey. Throughout my academic pursuit, my  passion for coding and web development has been the guiding light propelling me towards mastery in Full Stack Development.
+
+My time at SMU has not only allowed me to sharpen my technical skills but has also deepened my understanding of the intricate world of modern web development. Rooted in a strong foundation of HTML, CSS, and JavaScript, I specialize in crafting dynamic, user-focused interfaces that prioritize engaging user experiences.
+
+I am excited to channel my self-taught knowledge and academic background into meaningful contributions within innovative professional environments. A strong aspiration of mine is to employ my creative flair in designing intuitive and visually captivating web applications that make a lasting impact.
+
+With commitment to continuous learning and a passion for creating aesthetic and responsive designs, I am well-prepared to embrace fresh challenges and make valuable contributions to cutting-edge projects in the realm of Full Stack development. I welcome the opportunity to connect with discerning employers and explore collaborations aimed at crafting remarkable digital experiences together.        </p>
         <p>
-        Echo Park sriracha Pinterest vegan biodiesel 90's irony iPhone Kickstarter Carles crucifix kitsch narwhal dreamcatcher pickled trust fund selvage art party letterpress Tumblr post-ironic kogi Thundercats Tonx Brooklyn Pitchfork Odd Future authentic normcore freegan leggings Schlitz chambray organic tousled retro fap squid street art church-key fashion axe Wes Anderson bespoke whatever gentrify banh mi you probably haven't heard of them American Apparel next level pug ugh.
         </p>
       </div>
     </section>
--- Main/src/components/Resume/index.jsx
+++ Main/src/components/Resume/index.jsx
@@ -3,7 +3,7 @@ function Resume() {
     <section className="my-5">
       <div className="my-2">
       <p>
-        Download my <a href="https://www.linkedin.com/">resume</a>
+        Download my <a href="https://brown-veradis-30.tiiny.site">My Resume</a>
         </p>
         <h3>Front-end Proficiencies</h3>
         <ul className="skills">
