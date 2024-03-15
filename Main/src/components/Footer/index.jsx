720b6462ae934cb713970e35143a23b71c5e5cab
unknown <el_word9@yahoo.com>
2024-03-15 16:28:23 -0500

    added links

---
+++ Main/src/components/Footer/index.jsx
@@ -0,0 +1,30 @@
+function Footer() {
+
+  // Replace links with social media profiles
+  const icons = [
+    {
+      name: "fab fa-github",
+      link: "https://github.com/lsaox"
+    },
+    {
+      name: "fab fa-linkedin",
+      link: "https://www.linkedin.com/in/eliseorodriguezjr/"
+    },
+    {
+      name: "fab fa-stack-overflow",
+      link: "https://stackoverflow.com/users/23628868/eliseo-r"
+    }
+  ]
+
+  return (
+    <footer className="flex-row px-1">
+      {icons.map(icon =>
+      (
+        <a href={icon.link} key={icon.name} target="_blank" rel="noopener noreferrer"><i className={icon.name}></i></a>
+      )
+        )}
+    </footer>
+  );
+}
+
+export default Footer;
